c8a8d9a65a0a0d652034afab4b01e4f911cdbad5
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-04-17 11:40:48 +0200

    initial commit

---
+++ Example/Pods/Target Support Files/MustacheRxSwift/MustacheRxSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MustacheRxSwift : NSObject
+@end
+@implementation PodsDummy_MustacheRxSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-MustacheRxSwift_Example/Pods-MustacheRxSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MustacheRxSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MustacheRxSwift_Example
+@end
