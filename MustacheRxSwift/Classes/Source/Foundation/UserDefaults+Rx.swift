271427b0e2ff3537cc4cd493ef8b0da1d6887332
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-05-06 11:16:23 +0200

    Observe codable objects in userdefaults

---
+++ MustacheRxSwift/Classes/Source/Foundation/UserDefaults+Rx.swift
@@ -0,0 +1,20 @@
+//
+// Created by Tommy Hinrichsen on 2019-05-06.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public extension Reactive where Base: NSObject {
+
+    public func observeCodableWeakly<T: Codable>(_ type: T.Type, _ keyPath: String, options: KeyValueObservingOptions = [.new, .initial]) -> Observable<T?> {
+        return self.observeWeakly(Data.self, keyPath, options: options).map { data in
+            guard let data = data else { return nil }
+            let decoder = JSONDecoder()
+            let loaded = try? decoder.decode(T.self, from: data)
+            return loaded
+        }
+
+    }
+}
