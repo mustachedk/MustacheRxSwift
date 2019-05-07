8c046198d61d18f3a8c36da03a1fc61be1c1d944
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-05-07 13:11:14 +0200

    renamed method

--- MustacheRxSwift/Classes/Source/Foundation/UserDefaults+Rx.swift
+++ MustacheRxSwift/Classes/Source/Foundation/UserDefaults+Rx.swift
@@ -8,13 +8,12 @@ import RxCocoa
 
 public extension Reactive where Base: NSObject {
 
-    public func observeCodableWeakly<T: Codable>(_ type: T.Type, _ keyPath: String, options: KeyValueObservingOptions = [.new, .initial]) -> Observable<T?> {
-        return self.observeWeakly(Data.self, keyPath, options: options).map { data in
+    public func observeCodable<T: Codable>(_ type: T.Type, _ keyPath: String, options: KeyValueObservingOptions = [.new, .initial]) -> Observable<T?> {
+        return self.observe(Data.self, keyPath, options: options).map { data in
             guard let data = data else { return nil }
             let decoder = JSONDecoder()
             let loaded = try? decoder.decode(T.self, from: data)
             return loaded
         }
-
     }
 }
