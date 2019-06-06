624edfad908b7e8a1e17032d3722b9fdb39affda
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-06-06 09:49:44 +0200

    New method for observing userdefaults

--- MustacheRxSwift/Classes/Source/Foundation/UserDefaults+Rx.swift
+++ MustacheRxSwift/Classes/Source/Foundation/UserDefaults+Rx.swift
@@ -6,14 +6,19 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-public extension Reactive where Base: NSObject {
+public extension UserDefaults {
 
-    public func observeCodable<T: Codable>(_ type: T.Type, _ keyPath: String, options: KeyValueObservingOptions = [.new, .initial]) -> Observable<T?> {
-        return self.observe(Data.self, keyPath, options: options).map { data in
-            guard let data = data else { return nil }
+    public func observeCodable<T: Codable>(_ type: T.Type, _ keyPath: String) -> Observable<T?> {
+
+        let string = "\(UserDefaults.didChangeNotification.rawValue)-\(keyPath)"
+        let name = NSNotification.Name(rawValue: string)
+
+        return NotificationCenter.default.rx.notification(name).map { [keyPath]_ -> T? in
+            guard let data = self.value(forKey: keyPath) as? Data else { return nil }
             let decoder = JSONDecoder()
             let loaded = try? decoder.decode(T.self, from: data)
             return loaded
         }
     }
+
 }
