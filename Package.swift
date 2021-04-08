d22da5c02536a5578cfb17933f413943332855da
Tommy Sadiq Hinrichsen <th@mustache.dk>
2021-04-08 09:28:31 +0200

    Bumped RxSwift to 6.0.0

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ let package = Package(
     .library(name: "MustacheRxSwift", targets: ["MustacheRxSwift"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.0")),
-    .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git", .upToNextMajor(from: "5.1.0")),
-    .package(url: "https://github.com/devxoul/RxViewController.git", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+    .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git", .upToNextMajor(from: "6.0.0")),
+    .package(url: "https://github.com/devxoul/RxViewController.git", .upToNextMajor(from: "2.0.0")),
     .package(url: "https://github.com/mustachedk/MustacheServices", .upToNextMajor(from: "6.0.0")),
-    .package(url: "https://github.com/mustachedk/MustacheUIKit", .upToNextMajor(from: "2.0.0")),
+    .package(url: "https://github.com/mustachedk/MustacheUIKit", .upToNextMajor(from: "3.0.0")),
  ],
   targets: [
     .target(name: "MustacheRxSwift", dependencies: ["RxSwift", "RxSwiftExt", "RxViewController", "MustacheServices", "MustacheUIKit"])
